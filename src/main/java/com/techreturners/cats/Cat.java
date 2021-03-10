38baa87001ac89cab4e7612dee6d087878dd2a9d
shahanakhundmir <58731479+shahanakhundmir@users.noreply.github.com>
2021-03-10 20:24:46 +0000

    completed javaoo

--- src/main/java/com/techreturners/cats/Cat.java
+++ src/main/java/com/techreturners/cats/Cat.java
@@ -1,117 +1,15 @@
 package com.techreturners.cats;
 
 interface Cat {
-    public String sleeping = "Cat should be awake now";
     public String eat();
-    public String isAsleep();
-    //public void run();
-        //public sleep()
-    //run()
-}
-
-abstract class catBehaviour implements Cat{
-    public void goToSleep(){
-        sleeping = "Cat should be snoozing";
-    }
-    
-}
+    public boolean isAsleep();
+    public void goToSleep();
+    public void wakeUp();
+    public String getSetting();
+    public int getAverageHeight();
 
 
-class DomesticCat extends catBehaviour {
 
-    int averageHeight;
-    String setting;
-    String sleeping;
-
-    public DomesticCat(){
-        this.averageHeight = 23;
-        this.setting = "domestic";
-        this.sleeping = "Cat should be awake now";
-    }
-
-    public void goToSleep(){
-        this.sleeping = "Cat should be snoozing";
-    }
-    public void wakeUp(){
-        this.sleeping = "Cat should be awake now";
-    }
-    public String isAsleep() {
-        return sleeping;
-    }
-    
-    public String eat() {
-        return "Purrrrrrr";
-    }
-    public int getAverageHeight() {
-        return this.averageHeight;
-    }
-    public String getSetting() {
-        return this.setting;
-    }
-
-} // end of domestic cat
-
-
-class LionCat extends catBehaviour {
-
-    int averageHeight;
-    String setting;
-    String sleeping;
-
-    public LionCat(){
-        this.averageHeight = 1100;
-        this.setting = "wild";
-        this.sleeping = "Cat should be awake now";
-    }
-
-    public void goToSleep(){
-        this.sleeping = "Cat should be snoozing";
-    }
-    public void wakeUp(){
-        this.sleeping = "Cat should be awake now";
-    }
-    public String isAsleep() {
-        return sleeping;
-    }
-    public String eat() {
-        return "Roar!!!!";
-    }
-    public int getAverageHeight() {
-        return this.averageHeight;
-    }
-    public String getSetting() {
-        return this.setting;
-    }
-}
-
-class CheetahCat extends catBehaviour {
-
-    int averageHeight;
-    String setting;
-    String sleeping;
-
-    public CheetahCat(){
-        this.setting = "wild";
-        this.sleeping = "Cat should be awake now";
-    }
-    public void goToSleep(){
-        this.sleeping = "Cat should be snoozing";
-    }
-    public void wakeUp(){
-        this.sleeping = "Cat should be awake now";
-    }
-    public String isAsleep() {
-        return sleeping;
-    }
-    public String eat() {
-        return "Zzzzzzz";
-    }
-    public int getAverageHeight() {
-        return this.averageHeight;
-    }
-    public String getSetting() {
-        return this.setting;
-    }
 }
   
 
